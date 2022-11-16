7f5730df88febc384f2e339739c87a6c72e42e76
alialphasquad <muhammadali.alphasquad@gmail.com>
2022-11-16 20:13:01 +0500

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const user_1 = __importDefault(require("./route/user"));
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-let port = 5000;
+let port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-let port =5000;
+let port =process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors());
